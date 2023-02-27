00f70445f10815bf8a9f53949b6821e292e13808
IDTverdov <tidrocksil@yandex.ru>
2023-02-27 20:15:27 +0300

    add files

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+class Product(models.Model)
